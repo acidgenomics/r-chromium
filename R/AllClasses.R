af597c4fa32a036c15daa73b19a1b9c218ade928
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-14 17:45:23 -0500

    Improve documentation

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,13 +1,13 @@
 #' 10X Genomics Cell Ranger Data Set
 #'
 #' Extends `SingleCellExperiment`, with additional validity checks on the
-#' `metadata()` slot.
+#' `metadata` slot.
 #'
 #' @family S4 classes
 #' @author Michael Steinbaugh
 #' @export
 #'
-#' @seealso `CellRanger()`.
+#' @seealso `CellRanger`.
 setClass(
     Class = "CellRanger",
     contains = "SingleCellExperiment"
