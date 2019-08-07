ded2f056aa337d48582855db289ef6d741273166
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-07 15:57:09 -0400

    Prepare release

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -21,23 +21,26 @@ setValidity(
 
 
 
-#' Cell Ranger ATAC-seq data set
-#' 
-#' Contains single-cell ATAC-seq data.
-#'
-#' @author Michael Steinbaugh
-#' @export
-#' 
-#' @examples
-#' showClass("CellRangerATAC")
-setClass(
-    Class = "CellRangerATAC",
-    contains = "SingleCellExperiment"
-)
-setValidity(
-    Class = "CellRanger",
-    method = function(object) {
-        ## FIXME
-        stop("IN PROGRESS")
-    }
-)
+## nolint start
+## 
+## #' Cell Ranger ATAC-seq data set
+## #' 
+## #' Contains single-cell ATAC-seq data.
+## #'
+## #' @author Michael Steinbaugh
+## #' @export
+## #' 
+## #' @examples
+## #' showClass("CellRangerATAC")
+## setClass(
+##     Class = "CellRangerATAC",
+##     contains = "SingleCellExperiment"
+## )
+## setValidity(
+##     Class = "CellRangerATAC",
+##     method = function(object) {
+##         TRUE
+##     }
+## )
+## 
+## nolint end
--- R/importCounts-internal.R
+++ R/importCounts-internal.R
@@ -1,145 +1,13 @@
-#' Find the count matrix inside a Cell Ranger sample directory.
-#' 
-#' This function will automatically move inside an `outs/` directory inside
-#' the path if it is detected.
-#' 
-#' Currently preferring HDF5 over MTX.
-#' 
-#' @section File name history:
-#' 
-#' Cell Ranger v3
-#' 
-#' - H5: `filtered_feature_bc_matrix.h5`.
-#' - MTX: `filtered_feature_bc_matrix/matrix.mtx.gz`.
-#' 
-#' Cell Ranger v2
-#' 
-#' - H5: `filtered_gene_bc_matrices_h5.h5`
-#' - MTX: `filtered_gene_bc_matrices/<genomeBuild>/matrix.mtx`
-#' 
-#' @param dir Sample directory.
-#' @param filtered `logical(1)`.
-#'   - `TRUE`: Look for `filtered_*` matrix.
-#'   - `FALSE`: Look for `raw_*` matrix.
-#'   
-#'   Doesn't apply if there's only a single matrix file in the directory.
-#' 
-#' @note Updated 2019-08-07.
-#' @noRd
-.findCountMatrix <- function(dir, filtered = TRUE) {
-    assert(
-        isADirectory(dir),
-        isFlag(filtered)
-    )
-    dir <- realpath(dir)
-    
-    ## Simple mode -------------------------------------------------------------
-    ## For minimal examples and data downloaded from 10X website.
-    if (!dir.exists(file.path(dir, "outs"))) {
-        file <- list.files(
-            path = dir,
-            pattern = "matrix\\.(h5|mtx)(\\.gz)?",
-            full.names = TRUE
-        )
-        if (isAFile(file)) return(file)
-    }
-    
-    ## Standard Cell Ranger output ---------------------------------------------
-    ## Recurse into `outs/` directory by default.
-    dir <- file.path(dir, "outs")
-    assert(isADirectory(dir))
-    
-    if (isTRUE(filtered)) {
-        prefix <- "filtered"
-    } else {
-        prefix <- "raw"
-    }
-    
-    files <- list.files(
-        path = dir,
-        pattern = paste0("^", prefix, "_"),
-        recursive = FALSE,
-        full.names = FALSE
-    )
-    assert(hasLength(files))
-    
-    ## Get the Cell Ranger version, based on the file names.
-    if (isTRUE(any(grepl(
-        pattern = paste0("^", prefix, "_feature_bc_matrix$"),
-        x = files
-    )))) {
-        version <- "3"
-        filestem <- paste0(prefix, "_feature_bc_matrix")
-    } else if (isTRUE(any(grepl(
-        pattern = paste0("^", prefix, "_gene_bc_matrices$"),
-        x = files
-    )))) {
-        version <- "2"
-        filestem <- paste0(prefix, "_gene_bc_matrices")
-    } else {
-        stop("Failed to detect Cell Ranger version based on file names.")
-    }
-    version <- numeric_version(version)
-    
-    ## Currently preferring HDF5 over MTX.
-    if (isTRUE(
-        file.exists(file.path(dir, paste0(filestem, ".h5")))
-    )) {
-        ## v3 HDF5
-        file <- file.path(dir, paste0(filestem, ".h5"))
-    } else if (isTRUE(
-        file.exists(file.path(dir, paste0(filestem, "_h5.h5")))
-    )) {
-        ## v2 HDF5
-        file <- file.path(dir, paste0(filestem, "_h5.h5"))
-    } else if (isTRUE(
-        file.exists(file.path(dir, filestem, "matrix.mtx.gz"))
-    )) {
-        ## v3 MTX
-        file <- file.path(dir, filestem, "matrix.mtx.gz")
-    } else if (isTRUE(
-        dir.exists(file.path(dir, filestem))
-    )) {
-        ## v2 MTX
-        ## Get the genome build from the first sample directory.
-        genomeBuild <- list.dirs(
-            path = dir[[1L]],
-            full.names = FALSE,
-            recursive = FALSE
-        )
-        assert(isString(genomeBuild))
-        file <- file.path(dir, genomeBuild, "matrix.mtx")
-    }
-    
-    assert(isAFile(file))
-    file
-}
-
-
+## FIXME Rework to use matrixFiles instead.
 
 #' Import counts from either HDF5 or MTX files.
 #' @note Updated 2019-08-01.
 #' @noRd
 .importCounts <- function(
-    sampleDirs,
-    filtered,
+    matrixFiles,
     BPPARAM = BiocParallel::SerialParam()
 ) {
-    assert(
-        allAreDirectories(sampleDirs),
-        hasValidNames(sampleDirs),
-        isFlag(filtered)
-    )
-    matrixFiles <- unlist(bplapply(
-        X = sampleDirs,
-        FUN = .findCountMatrix,
-        filtered = filtered,
-        BPPARAM = BPPARAM
-    ))
-    assert(
-        hasLength(unique(basename(matrixFiles)), n = 1L),
-        hasValidNames(matrixFiles)
-    )
+    assert(allAreFiles(matrixFiles))
     if (all(grepl("\\.H5", matrixFiles, ignore.case = TRUE))) {
         fun <- .importCountsFromHDF5
     } else if (all(grepl("\\.MTX", matrixFiles, ignore.case = TRUE))) {
@@ -156,7 +24,7 @@
     ## on an Azure Files mount over CIFS.
     list <- bpmapply(
         sampleID = names(matrixFiles),
-        file = matrixFile,
+        file = matrixFiles,
         FUN = function(sampleID, file) {
             counts <- fun(file)
             ## Strip index when all barcodes end with "-1".
@@ -299,13 +167,13 @@
             grepl(pattern = "\\.MTX", x = file, ignore.case = TRUE)
         )
         path <- dirname(realpath(file))
-        files <- list.files(
+        files <- sort(list.files(
             path = path,
             pattern = "*.tsv*",
             full.names = FALSE,
             recursive = FALSE,
             ignore.case = TRUE
-        )
+        ))
         
         counts <- readMM(file)
         assert(is(counts, "sparseMatrix"))
@@ -318,7 +186,8 @@
             x = files,
             value = TRUE
         )
-        assert(isString(rownamesFile))
+        rownamesFile <- file.path(path, rownamesFile)
+        assert(isAFile(rownamesFile))
         ## Note that `features.tsv` is tab delimited.
         ## v2: id, name
         ## v3: id, name, expression type
@@ -334,10 +203,10 @@
             x = files,
             value = TRUE
         )
-        assert(isString(colnamesFiles))
         colnamesFile <- file.path(path, colnamesFile)
+        assert(isAFile(colnamesFile))
         ## Note that `barcodes.tsv` is NOT tab delimited.
-        colnames <- read_lines(barcodesFile)
+        colnames <- read_lines(colnamesFile)
         
         assert(
             identical(length(rownames), nrow(counts)),
@@ -349,3 +218,6 @@
         
         counts
     }
+
+
+
