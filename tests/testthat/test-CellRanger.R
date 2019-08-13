901dc6844180162033ce8d598cb8601273e06f38
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-13 13:17:29 -0400

    Add initial code coverage

--- tests/testthat/test-CellRanger.R
+++ tests/testthat/test-CellRanger.R
@@ -1,20 +1,20 @@
-context("Chromium")
+context("Cell Ranger v2")
 
-dir <- system.file("extdata/cellranger", package = "Chromium")
+dir <- system.file("extdata/cellranger_v2", package = "Chromium")
 
 ## Minimal mode, with no metadata or annotations.
 ## This is fast but doesn't slot a lot of useful info.
 test_that("Minimal metadata", {
-    x <- Chromium(dir = dir)
-    expect_s4_class(x, "Chromium")
+    x <- CellRanger(dir = dir)
+    expect_s4_class(x, "CellRanger")
 })
 
 ## Automatic organism annotations from AnnotationHub.
 test_that("AnnotationHub metadata", {
-    x <- Chromium(
+    x <- CellRanger(
         dir = dir,
         organism = "Homo sapiens",
         ensemblRelease = 87L
     )
-    expect_s4_class(x, "Chromium")
+    expect_s4_class(x, "CellRanger")
 })
