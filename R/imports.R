f77f9f56f560b41f8b6978f15981e0e6be84b874
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-21 13:32:39 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -1,22 +1,24 @@
 ## FIXME Need to move readSampleData to basejump.
 ## #' @importFrom bcbioBase readSampleData
 
+## FIXME Rework using basejump import
+## #' @importFrom readr read_lines read_tsv
+
 
 
 #' @importFrom BiocParallel SerialParam bplapply bpmapply bpparam
-#' @importFrom Matrix readMM sparseMatrix
+#' @importFrom Matrix sparseMatrix
 #' @importFrom SingleCellExperiment SingleCellExperiment isSpike
 #' @importFrom SummarizedExperiment assays
 #' @importFrom S4Vectors SimpleList mcols
-#' @importFrom basejump calculateMetrics camel detectLanes droplevels emptyRanges import
-#'   makeDimnames makeGRangesFromEnsembl makeGRangesFromGFF makeDimnames
-#'   makeNames makeSingleCellExperiment mapCellsToSamples minimalSampleData
-#'   realpath
+#' @importFrom basejump calculateMetrics camel detectLanes droplevels
+#'   emptyRanges import makeDimnames makeGRangesFromEnsembl makeGRangesFromGFF
+#'   makeDimnames makeNames makeSingleCellExperiment mapCellsToSamples
+#'   minimalSampleData readSampleData realpath
 #' @importFrom goalie allAreDirectories allAreFiles assert hasLength hasNames
 #'   hasValidDimnames hasValidNames isADirectory isAFile isAny isCharacter
 #'   isFlag isInt isNonEmpty isScalar isString isSubset validNames
 #' @importFrom methods as is new setClass setMethod setValidity validObject
-#' @importFrom readr read_lines read_tsv
 #' @importFrom rhdf5 h5dump h5read
 #' @importFrom stringr str_match str_split str_trunc
 #' @importFrom utils globalVariables packageVersion
