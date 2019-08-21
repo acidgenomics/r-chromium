656b7d84ab04f84047c2c3e7e5d30060bf9776ba
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-21 13:34:42 -0400

    Remove FIXME comments

--- R/package.R
+++ R/package.R
@@ -1,13 +1,3 @@
-## FIXME Need to fix these functions shared with bcbioSingleCell:
-## - .updateMetadata
-## - calculateMetrics
-## - readSampleData
-## Look for `requireNamespace` calls and improve...
-
-## FIXME Need to slot which version of Chromium in the object.
-
-
-
 #' Chromium
 #' 
 #' Toolkit for 10X Genomics Chromium single cell data.
