e4968ed42f12fe36c340f5bf0ffacea24b0f4866
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-21 17:00:03 -0400

    Pass linter checks

--- R/assert-internal.R
+++ R/assert-internal.R
@@ -1,13 +1,13 @@
 #' Does a given directory path contain a subdirectory name?
-#' 
+#'
 #' @param paths `character`.
 #'   Directory paths. Parameterized.
 #' @param name `character(1)`.
 #'   Subdirectory name.
-#' 
+#'
 #' @examples
 #' .hasSubdir(path = "sample_0001", name = "SC_RNA_COUNTER_CS")
-#' 
+#'
 #' @noRd
 .hasSubdir <- function(paths, name) {
     assert(
@@ -30,22 +30,22 @@
 
 
 #' Does the dataset contain a single sample?
-#' 
+#'
 #' This is a utility function intended to make loading of example datasets
 #' from the 10X Genomics website easier.
-#' 
+#'
 #' @param dir Cell Ranger output directory.
-#' 
+#'
 #' @note Updated 2019-08-01.
 #' @noRd
 .isSingleSample <- function(dir) {
     assert(isADirectory(dir))
-    
+
     ## Check for matrix in the top level of the directory.
     files <- list.files(path = dir, recursive = FALSE)
     ok <- any(grepl(pattern = "matrix\\.(h5|mtx)(\\.gz)?$", x = files))
     if (isTRUE(ok)) return(ok)
-    
+
     ## Check matrix files in `outs/` subdirectory.
     outsDir <- file.path(dir, "outs")
     if (dir.exists(outsDir)) {
@@ -54,6 +54,6 @@
         ok <- any(grepl("^(filtered|raw)_", files))
         if (isTRUE(ok)) return(ok)
     }
-    
+
     FALSE
 }
