69d553dc5f51e2debf768889b2fea7cd253ba8ee
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-22 15:29:51 -0400

    Resave example data

--- data-raw/pbmc_v2.R
+++ data-raw/pbmc_v2.R
@@ -67,6 +67,23 @@ tarfile <- file.path(dir, paste0(prefix, "_filtered_gene_bc_matrices.tar.gz"))
 untar(tarfile = tarfile, exdir = outs_dir)
 stopifnot(identical(dir(outs_dir), "filtered_gene_bc_matrices"))
 
+## Copy the example outs files.
+files <- c(
+    "metrics_summary.csv",
+    "molecule_info.h5"
+)
+invisible(lapply(
+    X = files,
+    FUN = function(file) {
+        file.copy(
+            from = file.path(dir, paste0(prefix, "_", file)),
+            to = file.path(outs_dir, file),
+            overwrite = TRUE
+        )
+        
+    }
+))
+
 ## Using Ensembl 84 GTF annotations.
 gtf_file <- file.path(data_raw_dir, "Homo_sapiens.GRCh38.84.gtf.gz")
 if (!file.exists(gtf_file)) {
@@ -98,77 +115,76 @@ assignAndSaveData(
 
 ## Example object ==============================================================
 counts <- counts(object)
-
 ## Subset the matrix to include only the top genes and cells.
 top_genes <- Matrix::rowSums(counts) %>%
     sort(decreasing = TRUE) %>%
     head(n = 500L)
 genes <- sort(names(top_genes))
-
 top_cells <- Matrix::colSums(counts) %>%
     sort(decreasing = TRUE) %>%
     head(n = 100L)
 cells <- sort(names(top_cells))
-
 ## Subset the original object dataset to contain only top genes and cells.
 object <- object[genes, cells]
-
 ## Report the size of each slot in bytes.
 lapply(coerceS4ToList(object), object_size)
 object_size(object)
 stopifnot(object_size(object) < limit)
 stopifnot(validObject(object))
-
 pbmc_v2 <- object
 usethis::use_data(pbmc_v2, compress = "xz", overwrite = TRUE)
 
 ## Example Cell Ranger v2 output ===============================================
-input_dir <- file.path(
-    outs_dir,
+input_dir <- dir
+input_sample_dir <- sample_dir
+input_outs_dir <- outs_dir
+input_matrix_dir <- file.path(
+    input_outs_dir,
     "filtered_gene_bc_matrices",
     "GRCh38"
 )
-stopifnot(dir.exists(input_dir))
+unlink(output_dir, recursive = TRUE)
 output_dir <- file.path(
     "inst",
     "extdata",
     "cellranger_v2"
 )
-unlink(output_dir, recursive = TRUE)
-sample_dir <- initDir(file.path(output_dir, "pbmc"))
-outs_dir <- initDir(file.path(sample_dir, "outs"))
-## Touch an empty file in the counter directory.
-counter_dir <- file.path(sample_dir, "SC_RNA_COUNTER_CS")
-initDir(counter_dir)
-file.create(file.path(counter_dir, "empty"))
-output_dir <- file.path(
-    outs_dir,
+output_sample_dir <- initDir(file.path(output_dir, "pbmc"))
+output_outs_dir <- initDir(file.path(output_sample_dir, "outs"))
+output_counter_dir <- file.path(output_sample_dir, "SC_RNA_COUNTER_CS")
+initDir(output_counter_dir)
+file.create(file.path(output_counter_dir, "empty"))
+output_matrix_dir <- file.path(
+    output_outs_dir,
     "filtered_gene_bc_matrices",
     "GRCh38"
 )
-initDir(output_dir)
-
+initDir(output_matrix_dir)
+## Copy metrics summary.
+file.copy(
+    from = file.path(input_outs_dir, "metrics_summary.csv"),
+    to = file.path(output_outs_dir, "metrics_summary.csv"),
+    overwrite = TRUE
+)
 ## Prepare the sparse matrix.
-counts <- Matrix::readMM(file = file.path(input_dir, "matrix.mtx"))
+counts <- Matrix::readMM(file = file.path(input_matrix_dir, "matrix.mtx"))
 counts <- counts[seq_len(100), seq_len(100)]
-Matrix::writeMM(counts, file = file.path(output_dir, "matrix.mtx"))
-
+Matrix::writeMM(counts, file = file.path(output_matrix_dir, "matrix.mtx"))
 genes <- read_tsv(
-    file = file.path(input_dir, "genes.tsv"),
+    file = file.path(input_matrix_dir, "genes.tsv"),
     col_names = FALSE,
     n_max = 100
 )
 write_tsv(
     x = genes,
-    path = file.path(output_dir, "genes.tsv"),
+    path = file.path(output_matrix_dir, "genes.tsv"),
     col_names = FALSE
 )
-
 barcodes <- read_lines(
-    file = file.path(input_dir, "barcodes.tsv"),
+    file = file.path(input_matrix_dir, "barcodes.tsv"),
     n_max = 100
 )
 write_lines(
     x = barcodes,
-    path = file.path(output_dir, "barcodes.tsv")
+    path = file.path(output_matrix_dir, "barcodes.tsv")
 )
