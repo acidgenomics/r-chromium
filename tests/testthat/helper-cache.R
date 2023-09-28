82f458c9c4e9e5289d3d1212ae36e8f6755b0f72
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 14:45:11 -0400

    Add notes on test data sources

--- tests/testthat/helper-cache.R
+++ tests/testthat/helper-cache.R
@@ -1,3 +1,14 @@
+## 10X Genomics example datasets:
+## - 4k PBMCs from a Healthy Donor
+##   Single Cell Gene Expression Dataset by Cell Ranger 2.1.0
+##   https://support.10xgenomics.com/single-cell-gene-expression/
+##     datasets/2.1.0/pbmc4k
+## - 5k Peripheral Blood Mononuclear Cells (PBMCs) from a Healthy Donor with a
+##   Panel of TotalSeq™-B Antibodies (v3 chemistry) Single Cell Gene Expression
+##   Dataset by Cell Ranger 3.1.0
+##   https://www.10xgenomics.com/resources/datasets/5-k-peripheral-blood-
+##     mononuclear-cells-pbm-cs-from-a-healthy-donor-with-cell-surface-proteins-
+##     v-3-chemistry-3-1-standard-3-1-0
 lst <- AcidDevTools::cacheTestFiles(
     pkg = .pkgName,
     files = c(
