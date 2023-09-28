13047e84355fe3ae96c158f6fd79b584c95616ba
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 15:50:35 -0400

    Improve coverage of datasets downloaded from 10X website

--- longtests/testthat/test-CellRanger.R
+++ longtests/testthat/test-CellRanger.R
@@ -1,15 +1,15 @@
-## FIXME Need to organize these better.
-## Extract the tar.gz files.
-
 test_that("v2 : HDF5", {
-    dir <- pbmc2
-    object <- CellRanger(dir)
+    dir <- dirs[["pbmc_v2_hdf5"]]
+    object <- CellRanger(dir, filtered = TRUE)
+    expect_s4_class(object, "CellRanger")
+    object <- CellRanger(dir, filtered = FALSE)
     expect_s4_class(object, "CellRanger")
 })
 
-## FIXME Rework this under longtests.
 test_that("v3 : HDF5", {
-    dir <- pbmc3
-    object <- CellRanger(dir)
+    dir <- dirs[["pbmc_v3_hdf5"]]
+    object <- CellRanger(dir, filtered = TRUE)
+    expect_s4_class(object, "CellRanger")
+    object <- CellRanger(dir, filtered = FALSE)
     expect_s4_class(object, "CellRanger")
 })
