bdd249e9fb2633d86ea1e1086bb703859bccd7e6
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 14:21:38 -0400

    Rework tests

--- tests/testthat/helper-cache.R
+++ tests/testthat/helper-cache.R
@@ -1,30 +1,11 @@
-if (!isTRUE(goalie::hasInternet())) {
-    warning("No Internet connection detected.")
-    return(invisible(NULL))
-}
-
-## pbmc_v2
-dir.create(
-    path = file.path("cache", "pbmc_v2"),
-    showWarnings = FALSE,
-    recursive = TRUE
+lst <- AcidDevTools::cacheTestFiles(
+    pkg = .pkgName,
+    files = c(
+        ## pbmc_v2.
+        "pbmc4k_raw_gene_bc_matrices_h5.h5",
+        ## pbmc_v3.
+        "5k_pbmc_protein_v3_filtered_feature_bc_matrix.h5"
+    )
 )
-file <- "pbmc4k_raw_gene_bc_matrices_h5.h5"
-url <- paste(ChromiumTestsURL, file, sep = "/")
-destfile <- file.path("cache", "pbmc_v2", file)
-if (!file.exists(destfile)) {
-    utils::download.file(url = url, destfile = destfile)
-}
-
-## pbmc_v3
-dir.create(
-    path = file.path("cache", "pbmc_v3"),
-    showWarnings = FALSE,
-    recursive = TRUE
-)
-file <- "5k_pbmc_protein_v3_filtered_feature_bc_matrix.h5"
-url <- paste(ChromiumTestsURL, file, sep = "/")
-destfile <- file.path("cache", "pbmc_v3", file)
-if (!file.exists(destfile)) {
-    utils::download.file(url = url, destfile = destfile)
-}
+cacheDir <- lst[["cacheDir"]]
+rm(lst)
